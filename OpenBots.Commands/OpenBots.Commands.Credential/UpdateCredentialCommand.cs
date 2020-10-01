692a8c457548420ff02a20abd0bc39b4c9487a4c
Francesca Faerman <ff@openbots.ai>
2020-10-01 18:02:28 -0400

    Nearly finished with credential commands

---
+++ OpenBots.Commands/OpenBots.Commands.Credential/UpdateCredentialCommand.cs
@@ -0,0 +1,89 @@
+﻿using OpenBots.Core.Attributes.ClassAttributes;
+using OpenBots.Core.Attributes.PropertyAttributes;
+using OpenBots.Core.Command;
+using OpenBots.Core.Enums;
+using OpenBots.Core.Infrastructure;
+using OpenBots.Core.Server.API_Methods;
+using OpenBots.Core.Utilities.CommonUtilities;
+using OpenBots.Engine;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace OpenBots.Commands.Credential
+{
+    [Serializable]
+    [Group("Credential Commands")]
+    [Description("This command gets an Credential from OpenBots Server")]
+    public class UpdateCredentialCommand : ScriptCommand
+    {
+        [PropertyDescription("Credential Name")]
+        [InputSpecification("Enter the name of the Credential")]
+        [SampleUsage("Name || {vCredentialName}")]
+        [Remarks("")]
+        [PropertyUIHelper(UIAdditionalHelperType.ShowVariableHelper)]
+        public string v_CredentialName { get; set; }
+
+        [PropertyDescription("Credential Username")]
+        [InputSpecification("Enter the Credential username")]
+        [SampleUsage("john@openbots.com || {vCredentialUsername}")]
+        [Remarks("")]
+        [PropertyUIHelper(UIAdditionalHelperType.ShowVariableHelper)]
+        public string v_CredentialUsername { get; set; }
+
+        [PropertyDescription("Credential Password")]
+        [InputSpecification("Enter the Credential password")]
+        [SampleUsage("john@openbots.com || {vCredentialPassword}")]
+        [Remarks("")]
+        [PropertyUIHelper(UIAdditionalHelperType.ShowVariableHelper)]
+        public string v_CredentialPassword { get; set; }
+
+        public UpdateCredentialCommand()
+        {
+            CommandName = "UpdateCredentialCommand";
+            SelectionName = "Update Credential";
+            CommandEnabled = true;
+            CustomRendering = true;
+        }
+
+        public override void RunCommand(object sender)
+        {
+            var engine = (AutomationEngineInstance)sender;
+            var vCredentialName = v_CredentialName.ConvertUserVariableToString(engine);
+            var vCredentialUsername = v_CredentialUsername.ConvertUserVariableToString(engine);
+            var vCredentialPassword = v_CredentialPassword.ConvertUserVariableToString(engine);
+
+            var client = new RestClient("https://openbotsserver-dev.azurewebsites.net/");
+
+            AuthMethods.GetAuthToken(client, "admin@admin.com", "Hello321");
+
+            var credential = CredentialMethods.GetCredential(client, $"name eq '{vCredentialName}'");
+
+            if (credential == null)
+                throw new Exception($"No Credential was found for '{vCredentialName}'");
+
+            credential.UserName = vCredentialUsername;
+            credential.PasswordSecret = vCredentialPassword;
+            //TODO update PasswordHash as well. API isn't doing it.
+            
+            CredentialMethods.PutCredential(client, credential);
+        }
+
+        public override List<Control> Render(IfrmCommandEditor editor, ICommandControls commandControls)
+        {
+            base.Render(editor, commandControls);
+
+            RenderedControls.AddRange(commandControls.CreateDefaultInputGroupFor("v_CredentialName", this, editor));
+            RenderedControls.AddRange(commandControls.CreateDefaultInputGroupFor("v_CredentialUsername", this, editor));
+            RenderedControls.AddRange(commandControls.CreateDefaultPasswordInputGroupFor("v_CredentialPassword", this, editor));
+
+            return RenderedControls;
+        }
+
+        public override string GetDisplayValue()
+        {
+            return base.GetDisplayValue() + $" [Update Credential '{v_CredentialName}']";
+        }       
+    }
+}
--- OpenBots.Core/Server/API Methods/CredentialMethods.cs
+++ OpenBots.Core/Server/API Methods/CredentialMethods.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using OpenBots.Server.Model;
+using RestSharp;
+using RestSharp.Serialization.Json;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OpenBots.Core.Server.API_Methods
 {
     public class CredentialMethods
     {
+        public static Credential GetCredential(RestClient client, string filter)
+        {
+            var request = new RestRequest("api/v1/Credentials", Method.GET);
+            request.AddParameter("$filter", filter);
+            request.RequestFormat = DataFormat.Json;
+
+            var response = client.Execute(request);
+            var deserializer = new JsonDeserializer();
+            var output = deserializer.Deserialize<Dictionary<string, string>>(response);
+            var items = output["items"];
+            return JsonConvert.DeserializeObject<List<Credential>>(items).FirstOrDefault();
+        }
+
+        public static void PutCredential(RestClient client, Credential credential)
+        {
+            var request = new RestRequest("api/v1/Credentials/{id}", Method.PUT);
+            request.AddUrlSegment("id", credential.Id.ToString());
+            request.RequestFormat = DataFormat.Json;
+            request.AddJsonBody(credential);
+
+            client.Execute(request);
+        }
     }
 }
