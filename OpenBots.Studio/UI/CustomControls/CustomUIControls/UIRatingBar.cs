4ce59b31a9c7be3e79075e189c36740b67ee6751
Francesca Faerman <ff@openbots.ai>
2020-11-10 10:00:30 -0500

    ratings control cleanup

--- OpenBots.Studio/UI/CustomControls/CustomUIControls/UIRatingBar.cs
+++ OpenBots.Studio/UI/CustomControls/CustomUIControls/UIRatingBar.cs
@@ -26,11 +26,11 @@ namespace OpenBots.UI.CustomControls.CustomUIControls
 
         private void SetupStars()
         {
-            Star1.Image = ImageList1.Images[_stars >= 1 ? "full" : "empty"];
-            Star2.Image = ImageList1.Images[_stars >= 2 ? "full" : "empty"];
-            Star3.Image = ImageList1.Images[_stars >= 3 ? "full" : "empty"];
-            Star4.Image = ImageList1.Images[_stars >= 4 ? "full" : "empty"];
-            Star5.Image = ImageList1.Images[_stars >= 5 ? "full" : "empty"];
+            Star1.Image = ImageList1.Images[Stars >= 1 ? "full" : "empty"];
+            Star2.Image = ImageList1.Images[Stars >= 2 ? "full" : "empty"];
+            Star3.Image = ImageList1.Images[Stars >= 3 ? "full" : "empty"];
+            Star4.Image = ImageList1.Images[Stars >= 4 ? "full" : "empty"];
+            Star5.Image = ImageList1.Images[Stars >= 5 ? "full" : "empty"];
         }
 
         private void Star1_Click(object sender, EventArgs e)
