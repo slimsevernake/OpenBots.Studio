0819bcfe6c736b23c7cabba4e614ed0b975402f1
openbots-su <74202865+openbots-su@users.noreply.github.com>
2020-11-30 14:39:46 -0500

    List command tests (#48)
    * Initial commit of AddListItm CreateLst and GetListCount tests
    
    * Implement tests for GetListItem, RemoveListItem, UpdateListItem
    
    * Removed unnecessary using statements
    
    * Add additional DataTable tests
    
    * Remove unnecessary Generic alias
    
    * Fix MailItem comparison by setting EmbedInteropTypes to false in reference properties
    
    * Add invalid input case for AddListItem tests

---
+++ OpenBots.Commands/OpenBots.Commands.List.Tests/GetListItemCommandTests.cs
@@ -0,0 +1,58 @@
+﻿using OpenBots.Core.Utilities.CommonUtilities;
+using OpenBots.Engine;
+using System.Collections.Generic;
+using Xunit;
+using System.Data;
+
+namespace OpenBots.Commands.List.Tests
+{
+    public class GetListItemCommandTests
+    {
+        private AutomationEngineInstance _engine;
+        private GetListItemCommand _getListItem;
+
+        [Fact]
+        public void GetsStringListItem()
+        {
+            _engine = new AutomationEngineInstance(null);
+            _getListItem = new GetListItemCommand();
+
+            List<string> list = new List<string>();
+            list.Add("item1");
+            list.Add("item2");
+            list.StoreInUserVariable(_engine, "{inputList}");
+
+            _getListItem.v_ListName = "{inputList}";
+            _getListItem.v_ItemIndex = "1";
+            _getListItem.v_OutputUserVariableName = "{output}";
+
+            _getListItem.RunCommand(_engine);
+
+            Assert.Equal("item2", "{output}".ConvertUserVariableToString(_engine));
+        }
+
+        [Fact]
+        public void GetsDataTableListItem()
+        {
+            _engine = new AutomationEngineInstance(null);
+            _getListItem = new GetListItemCommand();
+
+            List<DataTable> list = new List<DataTable>();
+            DataTable item1 = new DataTable();
+            item1.Columns.Add("d1col");
+            DataTable item2 = new DataTable();
+            item2.Columns.Add("d2col");
+            list.Add(item1);
+            list.Add(item2);
+            list.StoreInUserVariable(_engine, "{inputList}");
+
+            _getListItem.v_ListName = "{inputList}";
+            _getListItem.v_ItemIndex = "1";
+            _getListItem.v_OutputUserVariableName = "{output}";
+
+            _getListItem.RunCommand(_engine);
+
+            Assert.Equal(item2, (DataTable)"{output}".ConvertUserVariableToObject(_engine));
+        }
+    }
+}
---
+++ OpenBots.Commands/OpenBots.Commands.List.Tests/UpdateListItemCommandTests.cs
@@ -0,0 +1,95 @@
+﻿using OpenBots.Core.Utilities.CommonUtilities;
+using OpenBots.Engine;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using Xunit;
+
+namespace OpenBots.Commands.List.Tests
+{
+    public class UpdateListItemCommandTests
+    {
+        private AutomationEngineInstance _engine;
+        private UpdateListItemCommand _updateListItem;
+
+        [Fact]
+        public void UpdatesStringListItem()
+        {
+            _engine = new AutomationEngineInstance(null);
+            _updateListItem = new UpdateListItemCommand();
+
+            List<string> inputList = new List<string>();
+            inputList.Add("item1");
+            inputList.Add("item2");
+            string index = "0";
+            string item = "item3";
+
+            inputList.StoreInUserVariable(_engine, "{inputList}");
+            index.StoreInUserVariable(_engine, "{index}");
+            item.StoreInUserVariable(_engine, "{item}");
+
+            _updateListItem.v_ListName = "{inputList}";
+            _updateListItem.v_ListIndex = "{index}";
+            _updateListItem.v_ListItem = "{item}";
+
+            _updateListItem.RunCommand(_engine);
+
+            List<string> outputList = (List<string>)"{inputList}".ConvertUserVariableToObject(_engine);
+            Assert.Equal("item3", outputList[0]);
+        }
+
+        [Fact]
+        public void UpdatesDataTableListItem()
+        {
+            _engine = new AutomationEngineInstance(null);
+            _updateListItem = new UpdateListItemCommand();
+
+            List<DataTable> inputList = new List<DataTable>();
+            DataTable item1 = new DataTable();
+            item1.Columns.Add("d1col");
+            DataTable item2 = new DataTable();
+            item2.Columns.Add("d2col");
+            inputList.Add(item1);
+            inputList.Add(item2);
+            string index = "0";
+            DataTable newitem = new DataTable();
+            newitem.Columns.Add("d3col");
+
+            inputList.StoreInUserVariable(_engine, "{inputList}");
+            index.StoreInUserVariable(_engine, "{index}");
+            newitem.StoreInUserVariable(_engine, "{newitem}");
+
+            _updateListItem.v_ListName = "{inputList}";
+            _updateListItem.v_ListIndex = "{index}";
+            _updateListItem.v_ListItem = "{newitem}";
+
+            _updateListItem.RunCommand(_engine);
+
+            List<DataTable> outputList = (List<DataTable>)"{inputList}".ConvertUserVariableToObject(_engine);
+            Assert.Equal(newitem, outputList[0]);
+        }
+
+        [Fact]
+        public void HandlesInvalidListItem()
+        {
+            _engine = new AutomationEngineInstance(null);
+            _updateListItem = new UpdateListItemCommand();
+
+            List<DataTable> inputList = new List<DataTable>();
+            DataTable item1 = new DataTable();
+            string newItem = "item2";
+            inputList.Add(item1);
+            string index = "0";
+
+            inputList.StoreInUserVariable(_engine, "{inputList}");
+            index.StoreInUserVariable(_engine, "{index}");
+            newItem.StoreInUserVariable(_engine, "{item}");
+
+            _updateListItem.v_ListName = "{inputList}";
+            _updateListItem.v_ListIndex = "{index}";
+            _updateListItem.v_ListItem = "{item}";
+
+            Assert.Throws<Exception>(() => _updateListItem.RunCommand(_engine));
+        }
+    }
+}
