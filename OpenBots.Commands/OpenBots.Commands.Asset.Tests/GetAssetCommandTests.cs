cf176478ca4f13089e01cbf1262ac0644d7e2082
Samuel Uribe <su@openbots.ai>
2020-12-02 16:45:48 -0500

    Revert "Revert "GetAsset test initial commit""
    This reverts commit 7ff7a214f2e512075fe2c668c7f75c6ee646af7b.

---
+++ OpenBots.Commands/OpenBots.Commands.Asset.Tests/GetAssetCommandTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using OpenBots.Engine;
+using OpenBots.Core.Utilities.CommonUtilities;
+
+namespace OpenBots.Commands.Asset.Tests
+{
+    public class GetAssetCommandTests
+    {
+        private AutomationEngineInstance _engine;
+        private GetAssetCommand _getAsset;
+
+        [Fact]
+        public void GetsAsset()
+        {
+            _engine = new AutomationEngineInstance(null);
+            _getAsset = new GetAssetCommand();
+
+            _getAsset.v_AssetName = "name";
+            _getAsset.v_AssetType = "Text";
+            _getAsset.v_OutputDirectoryPath = "";
+            _getAsset.v_OutputUserVariableName = "";
+        }
+    }
+}
