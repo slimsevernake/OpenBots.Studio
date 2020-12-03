ae6b7fbccb2c54c5520f1402333c2779d2c87d08
Francesca Faerman <ff@openbots.ai>
2020-12-03 17:10:46 -0500

    fixed credential assembly info

--- OpenBots.Commands/OpenBots.Commands.Credential/Properties/AssemblyInfo.cs
+++ OpenBots.Commands/OpenBots.Commands.Credential/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OpenBots.Commands.Credential")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("OpenBots commands for interacting with Credentials stored on the OpenBots Server.Supports retrieving and updating existing Credentials.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("OpenBots.Commands.Credential")]
-[assembly: AssemblyCopyright("Copyright © 2020")]
+[assembly: AssemblyCopyright("(C) OpenBots Inc 2020-2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
