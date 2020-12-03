a36582ec9bbaf8206c0ed0ccba1acc618219d3bb
Samuel Uribe <su@openbots.ai>
2020-12-03 16:05:01 -0500

    Add additional tests for Asset commands

--- OpenBots.Commands/OpenBots.Commands.Asset.Tests/GetAssetCommandTests.cs
+++ OpenBots.Commands/OpenBots.Commands.Asset.Tests/GetAssetCommandTests.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using Xunit;
+using Xunit.Abstractions;
 using OpenBots.Engine;
 using OpenBots.Core.Utilities.CommonUtilities;
+using Newtonsoft.Json.Linq;
 
 namespace OpenBots.Commands.Asset.Tests
 {
@@ -13,9 +16,15 @@ namespace OpenBots.Commands.Asset.Tests
     {
         private AutomationEngineInstance _engine;
         private GetAssetCommand _getAsset;
+        private readonly ITestOutputHelper output;
+
+        public GetAssetCommandTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
 
         [Fact]
-        public void GetsAsset()
+        public void GetsTextAsset()
         {
             _engine = new AutomationEngineInstance(null);
             _getAsset = new GetAssetCommand();
@@ -28,6 +37,79 @@ namespace OpenBots.Commands.Asset.Tests
             _getAsset.RunCommand(_engine);
 
             Assert.Equal("testText", "{output}".ConvertUserVariableToString(_engine));
+        }
+
+        [Fact]
+        public void GetsNumberAsset()
+        {
+            _engine = new AutomationEngineInstance(null);
+            _getAsset = new GetAssetCommand();
+
+            _getAsset.v_AssetName = "testNumberAsset";
+            _getAsset.v_AssetType = "Number";
+            _getAsset.v_OutputDirectoryPath = "";
+            _getAsset.v_OutputUserVariableName = "{output}";
+
+            _getAsset.RunCommand(_engine);
+
+            var asset = "{output}".ConvertUserVariableToObject(_engine);
+
+            Assert.Equal("42", asset);
+        }
+
+        // We currently expect this test to fail due to Bug 3146
+        [Fact]
+        public void GetsJSONAsset()
+        {
+            _engine = new AutomationEngineInstance(null);
+            _getAsset = new GetAssetCommand();
+
+            _getAsset.v_AssetName = "testJSONAsset";
+            _getAsset.v_AssetType = "JSON";
+            _getAsset.v_OutputDirectoryPath = "";
+            _getAsset.v_OutputUserVariableName = "{output}";
+
+            _getAsset.RunCommand(_engine);
+
+            string jsonString = "{output}".ConvertUserVariableToString(_engine);
+            output.WriteLine(jsonString);
+            JObject jsonObject = JObject.Parse(jsonString);
+            Assert.Equal("testText", jsonObject["text"]);
+        }
+
+        [Fact]
+        public void GetsFileAsset()
+        {
+            _engine = new AutomationEngineInstance(null);
+            _getAsset = new GetAssetCommand();
+
+            string projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName;
+            string filepath = Path.Combine(projectDirectory, @"Resources\");
+
+            _getAsset.v_AssetName = "testFileAsset";
+            _getAsset.v_AssetType = "File";
+            _getAsset.v_OutputDirectoryPath = filepath;
+            _getAsset.v_OutputUserVariableName = "";
+
+            _getAsset.RunCommand(_engine);
+
+            Assert.True(File.Exists(filepath + "test.txt"));
+
+            File.Delete(filepath + "test.txt");
+        }
+
+        [Fact]
+        public void HandlesNonexistentAsset()
+        {
+            _engine = new AutomationEngineInstance(null);
+            _getAsset = new GetAssetCommand();
+
+            _getAsset.v_AssetName = "noAsset";
+            _getAsset.v_AssetType = "Text";
+            _getAsset.v_OutputDirectoryPath = "";
+            _getAsset.v_OutputUserVariableName = "{output}";
+
+            Assert.Throws<Exception>(() => _getAsset.RunCommand(_engine));
         }
     }
 }
---
+++ OpenBots.Commands/OpenBots.Commands.Asset.Tests/UpdateAssetCommandTests.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Xunit;
+using OpenBots.Engine;
+using OpenBots.Core.Utilities.CommonUtilities;
+
+namespace OpenBots.Commands.Asset.Tests
+{
+    public class UpdateAssetCommandTests
+    {
+        private AutomationEngineInstance _engine;
+        private UpdateAssetCommand _updateAsset;
+        private GetAssetCommand _getAsset;
+
+        [Fact]
+        public void UpdatesTextAsset()
+        {
+            _engine = new AutomationEngineInstance(null);
+            _updateAsset = new UpdateAssetCommand();
+            _getAsset = new GetAssetCommand();
+
+            string assetName = "testUpdateTextAsset";
+            string newAsset = "newText";
+            assetName.StoreInUserVariable(_engine, "{assetName}");
+            newAsset.StoreInUserVariable(_engine, "{newAsset}");
+
+
+            _updateAsset.v_AssetName = "{assetName}";
+            _updateAsset.v_AssetType = "Text";
+            _updateAsset.v_AssetFilePath = "";
+            _updateAsset.v_AssetValue = "{newAsset}";
+
+            _updateAsset.RunCommand(_engine);
+
+            _getAsset.v_AssetName = "{assetName}";
+            _getAsset.v_AssetType = "Text";
+            _getAsset.v_OutputUserVariableName = "{output}";
+
+            _getAsset.RunCommand(_engine);
+
+            string outputAsset = "{output}".ConvertUserVariableToString(_engine);
+            Assert.Equal("newText", outputAsset);
+
+            resetAsset(assetName, "oldText", "Text");
+        }
+
+        [Fact]
+        public void UpdatesNumberAsset()
+        {
+            _engine = new AutomationEngineInstance(null);
+            _updateAsset = new UpdateAssetCommand();
+            _getAsset = new GetAssetCommand();
+
+            string assetName = "testNumberAsset";
+            string newAsset = "70";
+            assetName.StoreInUserVariable(_engine, "{assetName}");
+            newAsset.StoreInUserVariable(_engine, "{newAsset}");
+
+
+            _updateAsset.v_AssetName = "{assetName}";
+            _updateAsset.v_AssetType = "Number";
+            _updateAsset.v_AssetFilePath = "";
+            _updateAsset.v_AssetValue = "{newAsset}";
+
+            _updateAsset.RunCommand(_engine);
+
+            _getAsset.v_AssetName = "{assetName}";
+            _getAsset.v_AssetType = "Number";
+            _getAsset.v_OutputUserVariableName = "{output}";
+
+            _getAsset.RunCommand(_engine);
+
+            string outputAsset = "{output}".ConvertUserVariableToString(_engine);
+            Assert.Equal("70", outputAsset);
+
+            resetAsset(assetName, "42", "Number");
+        }
+
+        [Fact]
+        public void UpdatesFileAsset()
+        {
+            _engine = new AutomationEngineInstance(null);
+            _updateAsset = new UpdateAssetCommand();
+            _getAsset = new GetAssetCommand();
+
+            string projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName;
+            string filepath = Path.Combine(projectDirectory, @"Resources\");
+            string assetName = "testUpdateFileAsset";
+            string newAsset = filepath + @"Upload\newtest.txt";
+            assetName.StoreInUserVariable(_engine, "{assetName}");
+            newAsset.StoreInUserVariable(_engine, "{newAsset}");
+
+
+            _updateAsset.v_AssetName = "{assetName}";
+            _updateAsset.v_AssetType = "File";
+            _updateAsset.v_AssetFilePath = newAsset;
+
+            _updateAsset.RunCommand(_engine);
+
+            _getAsset.v_AssetName = "{assetName}";
+            _getAsset.v_AssetType = "File";
+            _getAsset.v_OutputDirectoryPath = filepath + @"Download\";
+
+            _getAsset.RunCommand(_engine);
+
+            string outputAsset = "{output}".ConvertUserVariableToString(_engine);
+            Assert.True(File.Exists(filepath + @"Download\newtest.txt"));
+
+            File.Delete(filepath+@"Download\newtest.txt");
+            resetAsset(assetName, filepath + @"Upload\oldtest.txt", "File");
+        }
+
+        private void resetAsset(string assetName, string assetVal, string type)
+        {
+            _engine = new AutomationEngineInstance(null);
+            _updateAsset = new UpdateAssetCommand();
+
+            _updateAsset.v_AssetName = assetName;
+            _updateAsset.v_AssetType = type;
+            _updateAsset.v_AssetValue = assetVal;
+
+            if(type == "File")
+            {
+                _updateAsset.v_AssetFilePath = assetVal;
+            }
+
+            _updateAsset.RunCommand(_engine);
+        }
+    }
+}
